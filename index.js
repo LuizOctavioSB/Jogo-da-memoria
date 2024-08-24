32eb4a9915dd7f51222166d49251512f750ec756
LuizOctavioSB <luizoctavios14@gmail.com>
2024-08-24 15:50:33 -0300

    Melhoria do jogo e correção de bug

--- index.js
+++ index.js
@@ -33,7 +33,22 @@ let deck = [
     descricao: ["descricao 1", "descricao 2", "descricao 3"],
     virado: true,
   },
-
+  {
+    id: 5,
+    name: "sandy",
+    color: "rgb(117, 212, 246)",
+    imagem: "https://image.flaticon.com/icons/svg/3069/3069169.svg",
+    descricao: ["descricao 1", "descricao 2", "descricao 3"],
+    virado: true,
+  },
+  {
+    id: 6,
+    name: "gary",
+    color: "#84FAAC",
+    imagem: "https://image.flaticon.com/icons/svg/3069/3069169.svg",
+    descricao: ["descricao 1", "descricao 2", "descricao 3"],
+    virado: true,
+  }
 ];
 
 const cards = document.querySelectorAll('.card');
@@ -47,102 +62,102 @@ let score = 0;
 let pontuacao = 0;
 
 const btnRegras = document.querySelector('#btn-regras');
-
 const divRegras = document.querySelector('#regras');
+
 window.onload = () => {
-  // adiciona um event listener para exibir as regras quando o botão for clicado
-  btnRegras.addEventListener('click', () => {
-    divRegras.style.display = 'block';
+  // Mostra ou esconde a caixa de regras ao clicar no botão
+  btnRegras.addEventListener('click', (event) => {
+    event.stopPropagation(); // Impede que o clique se propague para outros elementos
+    divRegras.classList.toggle('hidden');
   });
 
-  // adiciona um event listener para esconder as regras quando o usuário clicar fora da caixa de regras
+  // Fecha a caixa de regras ao clicar em qualquer lugar fora dela
   document.addEventListener('click', (event) => {
-    if (!event.target.closest('#regras') && divRegras.style.display === 'block') {
-      divRegras.style.display = 'none';
+    if (!divRegras.classList.contains('hidden') && !event.target.closest('#regras')) {
+      divRegras.classList.add('hidden');
     }
   });
 }
 
 function flipCard() {
-  //this.classList.toggle('flip');
   if (lockBoard) return;
   if (this === firstCard) return;
 
   this.classList.add('flip');
 
-   if (!hasFlippedCard) {
-     hasFlippedCard = true;
-     firstCard = this;
-     return;
-    }
-    secondCard = this;
-    checkForMatch();
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+  secondCard = this;
+  checkForMatch();
 }
  
-  //Conferindo se é igual
+// Conferindo se é igual
 
-  function checkForMatch() {
-    movements++;
-    if (firstCard.dataset.nome === secondCard.dataset.nome) {
-      winContador++;
-      score += 10;
-      disableCards();
-      if (winContador == 4) {
-        pontuacao = score;
-        setTimeout(() => {
-          document.querySelector('#vitoria').style.display = 'block'
-        }, 1000);
-      }
-    } else {
-      score -= 5;
-      unflipCards();
+function checkForMatch() {
+  movements++;
+  if (firstCard.dataset.nome === secondCard.dataset.nome) {
+    winContador++;
+    score += 10;
+    disableCards();
+    if (winContador == 6) {
+      pontuacao = score;
+      setTimeout(() => {
+        document.querySelector('#vitoria').style.display = 'block'
+      }, 1000);
     }
-    updateScore();
+  } else {
+    score -= 5;
+    unflipCards();
   }
+  updateScore();
+}
  
-  //Desabilitando o clique nas cartas viradas
+// Desabilitando o clique nas cartas viradas
 
-  function disableCards() {
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
+}
+ 
+// Virando as cartas erradas de volta
+
+function unflipCards() {
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
 
     resetBoard();
-  }
- 
-  //Virando as cartas erradas de volta
+  }, 950);
+}
 
-  function unflipCards() {
-    lockBoard = true;
-    setTimeout(() => {
-      firstCard.classList.remove('flip');
-      secondCard.classList.remove('flip');
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
 
-      resetBoard();
-    }, 1500);
-  }
+// Embaralhando cartas (IIFE) Vai ser executada assim que for lida
 
-  function resetBoard() {
-    [hasFlippedCard, lockBoard] = [false, false];
-    [firstCard, secondCard] = [null, null];
-  }
+function shuffle() {
+  cards.forEach(card => {
+    let ramdomPos = Math.floor(Math.random() * 8);
+    card.style.order = ramdomPos;
+  });
+}
 
-  //Embaralhando cartas (IIFE) Vai ser executada assim que for lida
+function updateScore() {
+  const scoreElement = document.querySelector('#score');
+  scoreElement.innerText = `Score: ${score}`;
 
-  function shuffle() {
-    cards.forEach(card => {
-      let ramdomPos = Math.floor(Math.random() * 8);
-      card.style.order = ramdomPos;
-    });
-  }
-  
-  function updateScore() {
-    const scoreElement = document.querySelector('#score');
-    scoreElement.innerText = `Score: ${score}`;
-  
-    const finalScoreElement = document.querySelector('#pontuacao');
-    finalScoreElement.innerText = `Sua pontuação foi: ${pontuacao}`;
-  }
-  
-  shuffle();
-  cards.forEach(card => card.addEventListener('click', flipCard));
-  updateScore();
+  const finalScoreElement = document.querySelector('#pontuacao');
+  finalScoreElement.innerText = `Sua pontuação foi: ${pontuacao}`;
+}
+
+shuffle();
+cards.forEach(card => card.addEventListener('click', flipCard));
+updateScore();
